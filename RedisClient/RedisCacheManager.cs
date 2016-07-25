dafcca27876a663e00cae02cd6054bc1dcfa106f
saravanand <saravanan.d@techcello.com>
2016-07-25 22:41:16 +0530

    Redis with local cache added

--- RedisClient/RedisCacheManager.cs
+++ RedisClient/RedisCacheManager.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿
+using Newtonsoft.Json;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,6 +37,92 @@ namespace RedisClient
         public static void Delete(string key)
         {
             _db.StringSet(key, string.Empty);
+        }
+    }
+
+    public static class RedisWithLocalCacheManager
+    {
+        private static IDatabase _db = null;
+        private static System.Runtime.Caching.MemoryCache _memCache = MemoryCache.Default;
+        internal static ConnectionMultiplexer redis = null;
+
+        //static RedisWithLocalCacheManager()
+        //{
+        //    redis = ConnectionMultiplexer.Connect("localhost");
+        //    _db = RedisPubSubManager.redis.GetDatabase(0);
+        //}
+        public static void init()
+        {
+            redis = ConnectionMultiplexer.Connect("localhost");
+            _db = redis.GetDatabase(0);
+        }
+
+        public static void Add2Cache<T>(string key, T o)
+        {
+            var localKey = string.Format("{0}_time", key);
+
+            var ts = DateTime.UtcNow.Ticks.ToString();
+
+            _db.KeyDelete(localKey);
+            _db.StringSet(localKey, ts);
+            _memCache.Add(localKey, ts, new CacheItemPolicy());
+            _memCache.Add(key, o, new CacheItemPolicy());
+
+            var value = JsonConvert.SerializeObject(o);
+            _db.StringSet(key, value);
+        }
+
+        private static bool IsLocalFresh(string local, string server)
+        {
+            long srcTs = Convert.ToInt64(local), tgtTs = Convert.ToInt64(server);
+            return srcTs <= tgtTs;
+        }
+
+        public static T Get<T>(string key)
+        {
+            var localKey = string.Format("{0}_time", key);
+            var ts = DateTime.UtcNow.Ticks.ToString();
+
+            string serverTime = _db.StringGet(localKey);
+            string localTime = _memCache.Get(localKey) as string;
+
+            if (string.IsNullOrEmpty(serverTime)) // no data in server...
+            {
+                _memCache.Remove(key);
+                _memCache.Remove(localKey);
+                return default(T);
+            }
+
+            if (string.IsNullOrEmpty(localTime)) // server has data, local has no data
+            {
+                var serverString = _db.StringGet(key);
+                var serverData = JsonConvert.DeserializeObject<T>(serverString);
+                _memCache.Add(localKey, serverTime, new CacheItemPolicy());
+                _memCache.Add(key, serverData, new CacheItemPolicy());
+                return serverData;
+            }
+
+            if (IsLocalFresh(localTime, serverTime))
+                return (T)_memCache.Get(key);
+
+            var val = _db.StringGet(key);
+            if (!string.IsNullOrEmpty(val))
+            {
+                var result = JsonConvert.DeserializeObject<T>(val);
+                _memCache.Add(key, result, new CacheItemPolicy());
+                _memCache.Add(localKey, serverTime, new CacheItemPolicy());
+            }
+            return default(T);
+        }
+
+        public static void Delete(string key)
+        {
+            var localKey = string.Format("{0}_time", key);
+            _memCache.Remove(localKey);
+            _db.KeyDelete(localKey);
+
+            _memCache.Remove(key);
+            _db.KeyDelete(key);
         }
     }
 }
---
+++ RedisWithLocalCacheTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using RedisClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisWithLocalCacheTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisWithLocalCacheManager.init();
+
+            var d = new Data
+            {
+                Id = "ab",
+                Name = Path.GetRandomFileName()
+            };
+
+            RedisWithLocalCacheManager.Add2Cache("data_" + d.Id, d);
+
+            Console.WriteLine("call the other app to get the data and display");
+            Console.ReadLine();
+
+            Thread.Sleep(2500);
+
+            RedisWithLocalCacheManager.Delete("data_" + d.Id);
+            d.Id += "c";
+            RedisWithLocalCacheManager.Add2Cache("data_" + d.Id, d);
+            Thread.Sleep(2500);
+
+            Console.WriteLine("updated the data in the cache...");
+            Console.WriteLine("call the other app to read from cache and see if the data is ok i.e. id = abc");
+            Console.ReadLine();
+
+            d = RedisWithLocalCacheManager.Get<Data>("data_abcd");
+            Console.WriteLine(" id is : " + d.Id + " & name is : " + d.Name);
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public class Data
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
